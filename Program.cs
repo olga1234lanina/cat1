9ba9ad816416f317615df2f78f7a896cb434442d
Ольга <141999010+olga1234lanina@users.noreply.github.com>
2023-09-08 20:37:58 +0500

    Update Program.cs
    задача 27

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// задача 27. напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите пожалуйста число -> ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{num} -> {GetNumberSum(num)}");
+
+int GetNumberSum(int nums)
+{
+    int result = 0;
+    while (nums > 0)
+    {
+        result += nums % 10;
+        nums /= 10;
+    }
+    return result;
+}
